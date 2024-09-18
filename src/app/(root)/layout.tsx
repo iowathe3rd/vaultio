f15213f7ed7e18cfaf44d717d9a2df9fab35131a
Baurzhan Beglerov <bbeglerov@icloud.com>
2024-09-18 09:45:09 +0500

    feat(components): added some root layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,8 +1,30 @@
+import Header from "@/components/Header";
+import MobileNavigation from "@/components/MobileNavigation";
+import Sidebar from "@/components/Sidebar";
+import { Toaster } from "@/components/ui/toaster";
+import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+import React from "react";
 
-function Layout({children}: {children: React.ReactNode}) {
+export const dynamic = "force-dynamic";
+
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return redirect("/sign-in");
+
   return (
-    <div>{children}</div>
-  )
-}
+    <main className="flex h-screen">
+      <Sidebar {...currentUser} />
 
-export default Layout
+      <section className="flex h-full flex-1 flex-col">
+        <MobileNavigation {...currentUser} />
+        <Header userId={currentUser.$id} accountId={currentUser.accountId} />
+        <div className="main-content">{children}</div>
+      </section>
+
+      <Toaster />
+    </main>
+  );
+};
+export default Layout;
