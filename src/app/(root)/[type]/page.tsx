b67c961a3838cea905fd75950a5971954b23d6a8
Baurzhan Beglerov <bbeglerov@icloud.com>
2024-09-24 12:01:26 +0500

    feat(routes): added some content and file gathering to dynamic route

--- src/app/(root)/[type]/page.tsx
+++ src/app/(root)/[type]/page.tsx
@@ -1,8 +1,41 @@
+import Card from "@/components/Card";
+import { getFiles } from "@/lib/actions/file.actions";
+import { getFileTypesParams } from "@/lib/utils";
+import { Models } from "node-appwrite";
 
-const Page = () => {
+const Page = async ({ searchParams, params }: SearchParamProps) => {
+  const type = ((await params)?.type as string) || "";
+  const searchText = ((await searchParams)?.query as string) || "";
+  const sort = ((await searchParams)?.sort as string) || "";
+
+  const types = getFileTypesParams(type) as FileType[];
+
+  const files = await getFiles({ types, searchText, sort });
+
   return (
-    <div>Page</div>
-  )
-}
+    <div className="page-container">
+      <section className="w-full">
+        <h1 className="h1 capitalize">{type}</h1>
 
-export default Page
+        <div className="total-size-section">
+          <p className="body-1">
+            Total: <span className="h5">0 MB</span>
+          </p>
+        </div>
+      </section>
+
+      {/* Render the files */}
+      {files.total > 0 ? (
+        <section className="file-list">
+          {files.documents.map((file: Models.Document) => (
+            <Card key={file.$id} file={file} />
+          ))}
+        </section>
+      ) : (
+        <p className="empty-list">No files uploaded</p>
+      )}
+    </div>
+  );
+};
+
+export default Page;
